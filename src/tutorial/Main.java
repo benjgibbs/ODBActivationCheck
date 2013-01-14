6844443b328883e810a561053cfe89451c4e7f33
Ben <benjgibbs@gmail.com>
2013-01-14 19:24:04 +0000

    Initial commit - simple project to tell if objectdb is correctly activated in an objectdb.conf in the current directory.
    Based on the odb tutorial

---
+++ src/tutorial/Main.java
@@ -0,0 +1,38 @@
+package tutorial;
+ 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceException;
+ 
+public class Main {
+    public static void main(String[] args) {
+        // Open a database connection
+        // (create a new database if it doesn't exist yet):
+        EntityManagerFactory emf =
+            Persistence.createEntityManagerFactory("$objectdb/db/points.odb");
+        EntityManager em = emf.createEntityManager();
+ 
+        boolean allOkay = true;
+        try{
+        	em.getTransaction().begin();
+            for (int i = 0; i < 1_000_010; i++) {
+                Point p = new Point(i, i);
+                em.persist(p);
+            }
+            em.getTransaction().commit();	
+        }catch(PersistenceException pe){
+        	allOkay = false;
+        }finally {
+        	em.close();
+            emf.close();	
+        }
+        
+        if(allOkay){
+        	System.out.println("No problems.");
+        }else{
+        	System.out.println("Unregistered objectdb activation code");
+        }
+        
+    }
+}
